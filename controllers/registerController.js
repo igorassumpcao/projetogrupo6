895cda3a44e569844758687554b782e68e57c836
fhssouza <fhssouza@gmail.com>
2021-06-10 21:04:11 -0300

    validação de login

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -10,16 +10,19 @@ function get(req, res){
 async function post(req, res){
     const {email, password} = req.body
     const user = await registerModel.registerGet(email);
+    console.log('retorno do user', user)
 
     if (user){
         res.render('register')
+    }else if(!user){
+        const encryptPassword = bcrypt.hashSync(password, 12);
+
+        registerModel.insertLogin({email, password: encryptPassword});
+        
+        res.render('login');
     }
 
-    const encryptPassword = bcrypt.hashSync(password, 12);
-
-    registerModel.insertLogin({email, password: encryptPassword});
     
-    res.render('login');
 }
 
 module.exports = {
