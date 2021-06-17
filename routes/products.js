829fae64f8ef2905b6ab78697846c11fb1077dc5
Cesar <devcarlos.cesar@gmail.com>
2021-06-17 14:12:12 -0300

    adicionado Auth no botão 'ver mais'

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const productsController = require("../controllers/productsController");
 router.get("/", productsController.get);
 router.get("/edit/:id", authMiddleware.auth, productsController.edit);
 router.get("/types/:id", productsController.getByTypes);
-router.post("/types/:id", productsController.post);
+router.post("/types/:id", authMiddleware.auth, productsController.post);
 router.post("/", authMiddleware.auth, productsController.post);
 router.put("/:id", productsController.put);
 router.delete("/:id", authMiddleware.auth, productsController.delete);
